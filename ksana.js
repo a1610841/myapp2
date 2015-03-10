5bbee666316df49fb820f9ac4d87d8159bfadeb0
a1610841 <a1610841@gmail.com>
2015-03-10 15:17:40 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,48 @@
+jsonp_handler({
+ "version": "1",
+ "build": 4,
+ "title": "myapp2",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/a1610841/myapp2/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  547,
+  64,
+  652507,
+  145175,
+  277980,
+  825,
+  35607,
+  113502,
+  97542
+ ],
+ "filedates": [
+  "2015-03-10T06:55:24.022Z",
+  "2015-03-10T06:55:24.021Z",
+  "2015-03-10T06:55:24.128Z",
+  "2015-03-10T07:03:15.572Z",
+  "2015-03-10T06:55:24.062Z",
+  "2015-03-10T07:01:47.784Z",
+  "2015-03-10T06:55:24.016Z",
+  "2015-03-10T06:55:24.001Z",
+  "2015-03-10T06:55:39.469Z"
+ ],
+ "date": "2015-03-10T07:03:16.542Z"
+})
